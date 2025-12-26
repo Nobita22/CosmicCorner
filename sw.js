53157ebf957dea69db2bda8893aa01d3c790c4fa
Nobita <58627889+Nobita22@users.noreply.github.com>
2025-12-26 12:36:14 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,31 +1,9 @@
-const CACHE_NAME = 'CosmicCorner';
-const urlsToCache = [
-  './',
-  './index.html',
-  './manifest.json'
-];
+importScripts(
+  "https://storage.googleapis.com/workbox-cdn/releases/6.4.1/workbox-sw.js"
+);
 
-// Install the service worker
-self.addEventListener('install', function(event) {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        console.log('Opened cache');
-        return cache.addAll(urlsToCache);
-      })
-  );
-});
-
-// Fetch resources
-self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Return cached response if found, else fetch from network
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
-});
+// This will work!
+workbox.routing.registerRoute(
+  ({ request }) => request.destination === "image",
+  new workbox.strategies.NetworkFirst()
+);
